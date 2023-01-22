f4cdcc217caf5e311d08fafb55ac6dfad252370a
june <ajsjdlwj0123@gmail.com>
2023-01-22 15:47:35 +0900

    comment(Car):add new Comment to Car class

--- racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/Car.java
+++ racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/Car.java
@@ -1,31 +1,32 @@
 package racingcar.model.car;
 
-public class Car {
+public class Car {				
     private final Name name;
     private final Position position;
     private final GasTank gasTank;
-
+    
     public Car(final String name) {
         this.name = new Name(name);
         this.position = new Position();
         this.gasTank = new GasTank();
     }
     
-    static Car enrollWithName(final String name) {
+    static Car enrollWithName(final String name) { // 유저입력에 따른 Car 이름 설정
+    												// 유저 입력 예외처리는 Name 클래스가 처리
     	return new Car(name);
     }
     
-    public void forward() {
+    public void forward() {				// Car가 하는 것 - 포지션 이동
     	if(gasTank.isEnoughGas()) {
     		position.forward();
     	}
     }
     
-    public String getName() {
+    public String getName() {			// Car가 아는 것 자신의 이름
     	return name.toString();
     }
     
-    public String getPosition() {
+    public String getPosition() {		// Car가 아는 것 자신의 위치
     	return position.toString();
     }
 
