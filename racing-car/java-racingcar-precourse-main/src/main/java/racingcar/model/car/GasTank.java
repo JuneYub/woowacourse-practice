0e351057c65601c001ca7ab3a55caa648115086f
june <ajsjdlwj0123@gmail.com>
2023-01-22 13:26:36 +0900

    feat(GasTank):add GasTank class

--- racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/GasTank.java
+++ racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/GasTank.java
@@ -1,5 +1,16 @@
 package racingcar.model.car;
 
-public class GasTank {
+import racingcar.myutil.RandomUtils;
 
+public class GasTank {
+	private static final int MINIMUM_GAS = 0;
+	private static final int MAXIMUM_GAS = 9;
+	private static final int ENOUGH_GAS = 4;
+	
+	GasTank() {
+	}
+	
+	boolean isEnoughGas() {
+		return RandomUtils.nextPositiveInt(MINIMUM_GAS, MAXIMUM_GAS) >= ENOUGH_GAS;
+	}
 }
