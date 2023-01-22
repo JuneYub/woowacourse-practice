56d920c1697f82c9aea53272172eaf8763669efa
june <ajsjdlwj0123@gmail.com>
2023-01-22 13:27:07 +0900

    feat(RandomUtils):add RandomUtils class

--- racing-car/java-racingcar-precourse-main/src/main/java/racingcar/myutil/RandomUtils.java
+++ racing-car/java-racingcar-precourse-main/src/main/java/racingcar/myutil/RandomUtils.java
@@ -1,5 +1,27 @@
 package racingcar.myutil;
 
-public class RandomUtils {
+import java.util.Random;
 
+public class RandomUtils {
+	
+	private static final Random RANDOM = new Random();
+	
+	RandomUtils() {
+	}
+	
+	public static int nextPositiveInt(final int startInclusive, final int endInclusive) {
+		if(startInclusive > endInclusive) {
+			throw new IllegalArgumentException();
+		}
+		
+		if(startInclusive < 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		if(startInclusive == endInclusive) {
+			return startInclusive;
+		}
+		
+		return startInclusive + RANDOM.nextInt(endInclusive - startInclusive + 1); // 0에서 ~ 결과 사이의 값
+	}
 }
