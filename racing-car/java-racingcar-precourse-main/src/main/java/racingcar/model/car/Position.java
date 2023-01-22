5d1512617dd2aa9165c1f68bbee9b2eeeacdc0c2
june <ajsjdlwj0123@gmail.com>
2023-01-22 13:26:49 +0900

    feat(Position):add Position class

--- racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/Position.java
+++ racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/Position.java
@@ -1,5 +1,20 @@
 package racingcar.model.car;
 
 public class Position {
-
+	
+	private static final String UNIT_OF_POSITION = "-";
+	
+	private final StringBuilder position;
+	
+	Position() {
+		this.position = new StringBuilder();
+	}
+	
+	void forward() {
+		position.append(UNIT_OF_POSITION);
+	}
+	
+	public String toString() {
+		return position.toString();
+	}
 }
