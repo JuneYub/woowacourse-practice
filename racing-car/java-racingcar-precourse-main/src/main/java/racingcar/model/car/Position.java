522990ece5ae2b151d131cc1fb069501e47535db
june <ajsjdlwj0123@gmail.com>
2023-01-22 15:48:32 +0900

    comment(Position):add new comment to Position Class

--- racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/Position.java
+++ racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/car/Position.java
@@ -1,20 +1,20 @@
 package racingcar.model.car;
 
-public class Position {
+public class Position { // 자동차의 위치에 대한 원시값을 포장한 클래스
 	
-	private static final String UNIT_OF_POSITION = "-";
+	private static final String UNIT_OF_POSITION = "-"; // 바퀴 수 상수로 표현
 	
 	private final StringBuilder position;
 	
-	Position() {
+	Position() {		//	방어적 복사 원리에 따른 새로운 StringBuilder 생성
 		this.position = new StringBuilder();
 	}
 	
-	void forward() {
+	void forward() {			// 바퀴 수 추가
 		position.append(UNIT_OF_POSITION);
 	}
 	
-	public String toString() {
+	public String toString() {			// 마찬가지로 getter에 대한 방어적 복사되는지 안되는지 모르겠음
 		return position.toString();
 	}
 }
