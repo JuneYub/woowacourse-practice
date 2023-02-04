28175a7739e6f762e63779ed104b8abe6e5b6370
june <ajsjdlwj0123@gmail.com>
2023-02-05 02:15:02 +0900

    feat: add new OutPutView class

--- racing-car/java-racingcar-precourse-main/src/main/java/racingcar/view/OutPutView.java
+++ racing-car/java-racingcar-precourse-main/src/main/java/racingcar/view/OutPutView.java
@@ -1,5 +1,22 @@
 package racingcar.view;
 
-public class OutPutView {
+import java.util.List;
 
+public class OutPutView {
+	
+	public static void printStartRace() {
+		System.out.println(System.lineSeparator() + "실행결과");
+	}
+	
+	public static void printCarInfo(String name, String position) {
+		System.out.println(name + " : " + position);
+	}
+	
+	public static void printPassOneLap() {
+		System.out.println();
+	}
+	
+	public static void printWinners(List<String> winnerName) {
+		System.out.println(String.join(",", winnerName) + "가 최종 우승했습니다.");
+	}
 }
