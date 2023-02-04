d5c930dc68351f2a3c04893a68f0772c98e1492b
june <ajsjdlwj0123@gmail.com>
2023-02-05 02:15:38 +0900

    feat: add new Winners class

--- racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/Winners.java
+++ racing-car/java-racingcar-precourse-main/src/main/java/racingcar/model/Winners.java
@@ -1,5 +1,35 @@
 package racingcar.model;
 
-public class Winners {
+import java.util.ArrayList;
+import java.util.List;
 
+import racingcar.model.car.Car;
+
+public class Winners {
+	private Winners() {
+	}
+	
+	public static List<String> toList(final List<Car> cars) {
+		int maxPosition = 0;
+		List<String> winners = new ArrayList<>();
+		
+		for (Car car : cars) {
+			maxPosition = renewMaxPosition(maxPosition, winners, car);
+		}
+		
+		return winners;
+	}
+	
+	private static int renewMaxPosition(int maxPosition, final List<String> winners, final Car car) {
+		if(car.getPosition().length() < maxPosition) {
+			return maxPosition;
+		}
+		
+		if(car.getPosition().length() > maxPosition) {
+			winners.clear();
+			maxPosition = car.getPosition().length();
+		}
+		winners.add(car.getName());
+		return maxPosition;
+	}
 }
