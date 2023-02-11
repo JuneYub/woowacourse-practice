65a327954942cffd1d3bebf3d65b4d9235fddcbf
june <ajsjdlwj0123@gmail.com>
2023-02-11 12:27:00 +0900

    feat:add new LottoTicket class

--- java-lotto/src/main/java/lottogame/domain/ticket/LottoTicket.java
+++ java-lotto/src/main/java/lottogame/domain/ticket/LottoTicket.java
@@ -8,6 +8,32 @@ import java.util.Set;
 import lottogame.domain.number.LottoNumber;
 
 public class LottoTicket {
-
+	private static final int LOTTO_NUMBER_COUNT = 6;
+	
+	private final Set<LottoNumber> lottoNumbers;
+	
+	public LottoTicket(final Set<LottoNumber> lottoNumbers) {
+		this.lottoNumbers = new HashSet<>(lottoNumbers);
+		validateCount(this.lottoNumbers);
+	}
+	
+	private void validateCount(Set<LottoNumber> lottoNumbers) {
+		if(lottoNumbers.size() != LOTTO_NUMBER_COUNT) {
+			throw new IllegalArgumentException("유요한 로또 번호 개수가 아닙니다.");
+		}
+	}
+	
+	public List<Integer> getLottoNumbers() {
+		List<Integer> numberList = new ArrayList<>();
+		for(LottoNumber lottoNumber : this.lottoNumbers) {
+			numberList.add(lottoNumber.getNumber());
+		}
+		Collections.sort(nuberList);
+		return numberList;
+	}
+	
+	public boolean contatins(LottoNumber lottoNumber) {
+		return this.lottoNumber.contains(lottoNumber);
+	}
     
 }
